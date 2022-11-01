9418a0a6b49521c58583f7227e570b1e940e510c
João Dias <joaoeduardodias123@gmail.com>
2022-11-01 14:52:01 -0300

    feat: create component sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,33 @@
+import styles from './App.module.css'
 import { Header } from './components/Header'
+import { Sidebar } from './components/Sidebar'
 import './global.css'
 import { Post } from './Post'
 export function App() {
 
   return (
-    <>
+    <div>
     <Header />
+      <div className={styles.Wrapper}>
+        <Sidebar />
+       
+        <main>
+        <Post
+      author="João Dias"
+      content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti recusandae doloribus nam sit ad,
+               aspernatur rem, reiciendis dicta veritatis adipisci eaque aperiam fugiat! Error obcaecati praesentium in 
+               sapiente temporibus aliquam."    
+    />
     <Post
       author="João Dias"
       content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti recusandae doloribus nam sit ad,
                aspernatur rem, reiciendis dicta veritatis adipisci eaque aperiam fugiat! Error obcaecati praesentium in 
                sapiente temporibus aliquam."    
     />
+        </main>
+      </div>
    
-   </>
+   </div>
   )
 }
   
