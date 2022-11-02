e4fcd65c111de94616edc3d07484ba633e7775c9
joaoeduardodias <joaoeduardodias123@gmail.com>
2022-11-02 13:40:16 -0300

    feat create component avatar

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,10 +1,10 @@
 import { ThumbsUp, Trash } from "phosphor-react";
+import { Avatar } from "./Avatar";
 import styles from "./Comment.module.css";
-
 export function Comment() {
   return (
     <div className={styles.comment}>
-      <img src="https://github.com/joaoeduardodias.png" />
+      <Avatar hasBorder={false} src="https://github.com/joaoeduardodias.png" />
 
       <div className={styles.commentBox}>
         <div className={styles.commentContent}>
